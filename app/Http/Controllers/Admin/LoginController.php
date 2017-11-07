44497196637da8afb3cb67cacd79d2b469ba5312
lider <you@example.com>
2017-11-07 16:53:07 +0800

    add captcha

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Mews\Captcha\Facades\Captcha;
 
 class LoginController extends CommonController
 {
     public function login()
     {
         return view('admin.login');
+    }
+
+
+    public function code()
+    {
+        return Captcha::create('default');
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -24,14 +24,13 @@
 					<li>
 						<input type="text" class="code" name="code"/>
 						<span><i class="fa fa-check-square-o"></i></span>
-						<img src="#" alt="">
+						<img src="{{url('admin/code')}}" onClick="this.src='{{url('admin/code')}}?'+Math.random()" alt="">
 					</li>
 					<li>
 						<input type="submit" value="立即登陆"/>
 					</li>
 				</ul>
 			</form>
-			<p><a href="#">返回首页</a> &copy; 2016 Powered by <a href="http://www.houdunwang.com" target="_blank">http://www.houdunwang.com</a></p>
 		</div>
 	</div>
 </body>
