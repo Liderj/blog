fe222f0cef7662fd1f72a65c61683246f4c1d416
lider <you@example.com>
2017-11-07 17:28:04 +0800

    create blog_user  migrate

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 use Mews\Captcha\Facades\Captcha;
 
 class LoginController extends CommonController
 {
     public function login()
     {
-        return view('admin.login');
+        if ($input =Input::all()){
+            if(Captcha::check(Input::get('code'))){
+                return 'ok';
+            }else{
+                return back()->with('msg','验证码错误');
+            }
+        }else{
+            return view('admin.login');
+        }
     }
 
 
     public function code()
     {
         return Captcha::create('default');
+    }
+
+    public function crypt()
+    {
+        $str ='123456';
+        return encrypt($str);
     }
 }
--- database/migrations/2017_11_07_091914_create_blog_user_table.php
+++ database/migrations/2017_11_07_091914_create_blog_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateBlogUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('blog_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username',50)->comment('用户名');
+            $table->string('userpass',255)->comment('密码');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('blog_user');
     }
 }
